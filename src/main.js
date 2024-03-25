a37759467d48ffe68859710534028bac30fef49f
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-03-25 23:29:20 +0200

    update

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+import { fetchPhotoFromPixabay } from './js/pixabay-api';
+import { renderPhotos } from './js/render-functions';
+
+const form = document.querySelector('.search-form');
