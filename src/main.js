efa2db244ec29e6a35a88c1e4b13ef9c0000fe5b
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-04-03 23:11:53 +0300

    update main

--- src/main.js
+++ src/main.js
@@ -8,3 +8,15 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { fetchPhotoFromPixabay } from './js/pixabay-api';
 import { renderPhotos } from './js/render-functions';
+
+const loader = document.querySelector('.loader');
+const form = document.querySelector('.img-information');
+const searchInput = document.querySelector('.input-img-name');
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+function hideLoader() {
+  loader.style.display = 'none';
+}
+//loader.classList.add('hidden');
